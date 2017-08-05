a8c82b60476604adac0f2227c888b7fd7a34e065
Shofi Ul Alam <Shofi Ul Alam@DESKTOP-SPCMTJE>
2017-08-05 10:40:51 -0500

    Basic Java Classes

---
+++ Module2/src/Test_Java.java
@@ -0,0 +1,16 @@
+
+public class Test_Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		name();
+		
+	}
+
+	public static void name()
+	{
+		System.out.println("My name is Alam");
+	}
+	
+}
